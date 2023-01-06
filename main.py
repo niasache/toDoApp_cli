9d93b1897834297104c4549f4ce7284a87c1b5cc
niasache <niasache@gmail.com>
2023-01-06 16:31:34 -0500

    added functionality to the cli program

---
+++ main.py
@@ -0,0 +1,60 @@
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith('add'):
+        todo = user_action[4:]
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readLines()
+        
+        todos.appen(todo)
+
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    elif user_action.startswith('show'):
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+
+    elif user_action.startswith('edit'):
+        number = int(user_action[5:])
+        print(number)
+
+        index = number - 1
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        new_todo = input("Enter new todo: ")
+        todos[index] = new_todo + '\n'
+
+        with open('todos.txt', 'w'):
+            file.writelines(todos)
+
+    elif user_action.startswith('complete'):
+        number = int(user_action[9:])
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+        
+        index = number - 1
+
+        todo_to_remove = todos[index].strip('\n')
+        todos.pop(index)
+
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+
+        message = f"Todo {todo_to_remove} was removed from the list."
+        print(message)
+
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print("Command is not valid")
